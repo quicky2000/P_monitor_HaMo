9cd3c5926cbdd11aa344963c3300b8e604e82801
Julien Thevenon <julien_thevenon@yahoo.fake.fr>
2015-09-16 19:23:31 +0200

    Initial import

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -f
+mkdir build
+cd build
+mkdir monitor_HaMo
+cd monitor_HaMo
+generate_makefile monitor_HaMo
+make
+#EOF
+
---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+QUICKY_REPOSITORY=$PWD/sources
+export QUICKY_REPOSITORY
+cd $QUICKY_REPOSITORY/quicky_tools/setup
+. setup.sh
+cd $QUICKY_REPOSITORY/../
+#EOF
